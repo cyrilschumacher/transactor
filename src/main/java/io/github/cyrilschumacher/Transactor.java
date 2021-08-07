38b28d450ff813634bca0f740990c7721e219dba
Cyril Schumacher <cyril_schumacher@live.fr>
2021-08-07 13:51:05 +0200

    feat(transactor): add builder and parser

---
+++ src/main/java/io/github/cyrilschumacher/Transactor.java
@@ -0,0 +1,168 @@
+package io.github.cyrilschumacher;
+
+import io.github.cyrilschumacher.data.DataElement;
+import io.github.cyrilschumacher.data.codec.DataTypeCodecRegistry;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Transactor<T extends Enum<T> & DataElement> {
+
+    private static final int BITMAP_LENGTH = 8;
+    private static final int HEXADECIMAL_RADIX = 16;
+    private static final int MESSAGE_TYPE_INDICATOR_LENGTH = 4;
+
+    private final Bitmap bitmap;
+    private final MessageData<T> messageData;
+    private final int messageTypeIndicator;
+    private final Charset charset;
+
+    private Transactor(final int messageTypeIndicator, final Bitmap bitmap, final MessageData<T> messageData, final Charset charset) {
+        this.messageTypeIndicator = messageTypeIndicator;
+        this.bitmap = bitmap;
+        this.messageData = messageData;
+        this.charset = charset;
+    }
+
+    public static <T extends Enum<T> & DataElement> Builder<T> builder(final int messageTypeIndicator, final Charset charset) {
+        return builder(messageTypeIndicator, charset, new DataTypeCodecRegistry());
+    }
+
+    public static <T extends Enum<T> & DataElement> Builder<T> builder(final int messageTypeIndicator, final Charset charset, final DataTypeCodecRegistry dataTypeCodecRegistry) {
+        return new Builder<T>(messageTypeIndicator, dataTypeCodecRegistry, charset);
+    }
+
+    public static <T extends Enum<T> & DataElement> Transactor<T> parse(final byte[] data, final Class<T> klass, final Charset charset) {
+        return parse(data, klass, charset, new DataTypeCodecRegistry());
+    }
+
+    public static <T extends Enum<T> & DataElement> Transactor<T> parse(final byte[] data, final Class<T> klass, final Charset charset, final DataTypeCodecRegistry dataTypeCodecRegistry) {
+        final ByteBuffer buffer = ByteBuffer.wrap(data);
+
+        final int messageTypeIndicator = readMessageTypeIndicator(buffer, charset);
+        final Bitmap bitmap = readBitmap(buffer);
+        final MessageData<T> messageData = readMessage(buffer, klass, bitmap, charset, dataTypeCodecRegistry);
+
+        return new Transactor<>(messageTypeIndicator, bitmap, messageData, charset);
+    }
+
+    private static <T extends Enum<T> & DataElement> MessageData<T> readMessage(final ByteBuffer buffer, final Class<T> klass, final Bitmap bitmap, final Charset charset, final DataTypeCodecRegistry dataTypeCodecRegistry) {
+        final int length = buffer.remaining();
+        final byte[] data = new byte[length];
+        buffer.get(data);
+
+        final List<T> dataElements = Arrays.stream(klass.getEnumConstants()).sorted().filter(dataElement -> bitmap.getDataFields().contains(dataElement.getField())).collect(Collectors.toUnmodifiableList());
+        return MessageData.parse(dataElements, data, charset, dataTypeCodecRegistry);
+    }
+
+    private static Bitmap readBitmap(final ByteBuffer buffer) {
+        final byte[] data = new byte[BITMAP_LENGTH];
+        buffer.get(data);
+
+        return Bitmap.parse(data);
+    }
+
+    private static int readMessageTypeIndicator(final ByteBuffer buffer, final Charset charset) {
+        final byte[] data = new byte[MESSAGE_TYPE_INDICATOR_LENGTH];
+        buffer.get(data);
+
+        final String messageTypeIndicator = new String(data, charset);
+        return Integer.parseInt(messageTypeIndicator, HEXADECIMAL_RADIX);
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public Charset getCharset() {
+        return charset;
+    }
+
+    public MessageData<T> getMessageData() {
+        return messageData;
+    }
+
+    public int getMessageTypeIndicator() {
+        return messageTypeIndicator;
+    }
+
+    public byte[] toByteArray() throws IOException {
+        final byte[] messageTypeIndicatorByteArray = generateMessageTypeIndicator(messageTypeIndicator, charset);
+        final byte[] bitmap = this.bitmap.toByteArray();
+        final byte[] message = this.messageData.toByteArray();
+
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        buffer.write(messageTypeIndicatorByteArray);
+        buffer.write(bitmap);
+        buffer.write(message);
+
+        return buffer.toByteArray();
+    }
+
+    private byte[] generateMessageTypeIndicator(final int messageTypeIndicator, final Charset charset) {
+        final String messageTypeIndicatorString = Integer.toHexString(messageTypeIndicator);
+        final byte[] encodedValue = messageTypeIndicatorString.getBytes(charset);
+
+        return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).put(encodedValue).array();
+    }
+
+    public static class Builder<T extends Enum<T> & DataElement> {
+
+        private static final int SECONDARY_BITMAP_RANGE = 2;
+        private static final int TERTIARY_BITMAP_RANGE = 3;
+
+        private final Bitmap.Builder bitmapBuilder;
+        private final MessageData.Builder<T> messageBuilder;
+        private final int messageTypeIndicator;
+        private final Charset charset;
+
+        private Builder(final int messageTypeIndicator, final DataTypeCodecRegistry dataTypeCodecRegistry, final Charset charset) {
+            this(
+                messageTypeIndicator,
+                Bitmap.builder(),
+                MessageData.<T>builder().withCharset(charset).withDataTypeCodecRegistry(dataTypeCodecRegistry),
+                charset
+            );
+        }
+
+        private Builder(final int messageTypeIndicator, final Bitmap.Builder bitmapBuilder, final MessageData.Builder<T> messageBuilder, final Charset charset) {
+            this.messageTypeIndicator = messageTypeIndicator;
+            this.bitmapBuilder = bitmapBuilder;
+            this.messageBuilder = messageBuilder;
+            this.charset = charset;
+        }
+
+        public <V> Builder<T> add(final T element, final V value) {
+            final int dataField = element.getField();
+            bitmapBuilder.add(dataField);
+            messageBuilder.add(element, value);
+
+            return this;
+        }
+
+        public <V> Builder<T> hasSecondaryBitmap() {
+            bitmapBuilder.withRange(SECONDARY_BITMAP_RANGE);
+            return this;
+        }
+
+        public <V> Builder<T> hasTertiaryBitmap() {
+            bitmapBuilder.withRange(TERTIARY_BITMAP_RANGE);
+            return this;
+        }
+
+        public Transactor<T> build() {
+            final Bitmap bitmap = bitmapBuilder.build();
+            final MessageData<T> messageData = messageBuilder.build();
+
+            return new Transactor<>(messageTypeIndicator, bitmap, messageData, charset);
+        }
+
+    }
+
+}
---
+++ src/test/java/io/github/cyrilschumacher/TransactorTest.java
@@ -0,0 +1,112 @@
+package io.github.cyrilschumacher;
+
+import jakarta.xml.bind.DatatypeConverter;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Unit tests for the class: {@link Transactor}.
+ */
+@DisplayName("Unit tests for `Transactor` class.")
+class TransactorTest {
+
+    @Nested
+    @DisplayName("When a message is created.")
+    class WhenMessageIsCreated {
+
+        @Test
+        @DisplayName("Should create a message.")
+        void shouldCreateMessage() throws IOException {
+            // Given
+            var transmissionDateTime = ZonedDateTime.of(2021, 8, 2, 15, 22, 21, 0, ZoneOffset.UTC);
+            var dateTimeLocalTransaction = LocalDateTime.of(2021, 8, 2, 15, 22, 21);
+            var personalIdentificationData = new byte[]{(byte) 0xA8, (byte) 0xEC, (byte) 0xBC, 0x46, (byte) 0xAD, (byte) 0xEF, (byte) 0xD3, (byte) 0x8F};
+
+            var transactor = Transactor.<DummyDataElement>builder(0x1100, StandardCharsets.US_ASCII)
+                    .add(DummyDataElement.PRIMARY_ACCOUNT_NUMBER, "4552119306245327")
+                    .add(DummyDataElement.PROCESSING_CODE, "40")
+                    .add(DummyDataElement.AMOUNT, "124")
+                    .add(DummyDataElement.TRANSMISSION_DATE_TIME, transmissionDateTime)
+                    .add(DummyDataElement.SYSTEM_TRACE_AUDIT_NUMBER, "101")
+                    .add(DummyDataElement.DATE_TIME_LOCAL_TRANSACTION, dateTimeLocalTransaction)
+                    .add(DummyDataElement.DATE_EXPIRATION, YearMonth.of(2020, 10))
+                    .add(DummyDataElement.POINT_OF_SERVICE_DATA_CODE, "V69999000000")
+                    .add(DummyDataElement.FUNCTION_CODE, "101")
+                    .add(DummyDataElement.CARD_ACCEPTOR_BUSINESS_CODE, "1520")
+                    .add(DummyDataElement.ACQUIRER_INSTITUTION_ID_CODE, "456654120087")
+                    .add(DummyDataElement.RETRIEVAL_REFERENCE_NUMBER, "099920700619")
+                    .add(DummyDataElement.CARD_ACCEPTOR_TERMINAL_IDENTIFICATION, "43214321")
+                    .add(DummyDataElement.CARD_ACCEPTOR_ID_CODE, "455490051556622")
+                    .add(DummyDataElement.CARD_ACCEPTOR_NAME_LOCATION, "Welch & Cie\\Geraldine Lane\\New York\\11590")
+                    .add(DummyDataElement.CURRENCY_CODE, "978")
+                    .add(DummyDataElement.PERSONAL_IDENTIFICATION_DATA, personalIdentificationData)
+                    .build();
+
+            // When
+            var message = transactor.toByteArray();
+
+            // Then
+            var expectedData = DatatypeConverter.parseHexBinary("313130307230054108E0900031363435353231313933303632343533323730303030343030303030303030303031323430383032313532323231303030313031323130383032313532323231323031305636393939393030303030303130313135323031323435363635343132303038373039393932303730303631393433323134333231343535343930303531353536363232343157656C63682026204369655C476572616C64696E65204C616E655C4E657720596F726B5C3131353930393738A8ECBC46ADEFD38F");
+            MatcherAssert.assertThat(message, equalTo(expectedData));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("When the message is parsed.")
+    class WhenMessageIsParsed {
+
+        @Test
+        @DisplayName("Should parse a message.")
+        void shouldParseMessage() {
+            // Given
+            var data = DatatypeConverter.parseHexBinary("313130307230054108E0900031363435353231313933303632343533323730303030343030303030303030303031323430383032313532323231303030313031323130383032313532323231323031305636393939393030303030303130313135323031323435363635343132303038373039393932303730303631393433323134333231343535343930303531353536363232343157656C63682026204369655C476572616C64696E65204C616E655C4E657720596F726B5C3131353930393738A8ECBC46ADEFD38F");
+
+            // When
+            var transactor = Transactor.parse(data, DummyDataElement.class, StandardCharsets.US_ASCII);
+
+            // Then
+            var expectedPersonalIdentificationData = new byte[]{(byte) 0xA8, (byte) 0xEC, (byte) 0xBC, 0x46, (byte) 0xAD, (byte) 0xEF, (byte) 0xD3, (byte) 0x8F};
+
+            MatcherAssert.assertThat(transactor.getMessageTypeIndicator(), equalTo(0x1100));
+
+            var bitmap = transactor.getBitmap();
+            MatcherAssert.assertThat(bitmap.getDataFields(), containsInAnyOrder(2, 3, 4, 7, 11, 12, 22, 24, 26, 32, 37, 41, 42, 43, 49, 52));
+            MatcherAssert.assertThat(bitmap.getRange(), equalTo(1));
+
+            var messageData = transactor.getMessageData();
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.PRIMARY_ACCOUNT_NUMBER, String.class), equalTo("4552119306245327"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.PROCESSING_CODE, String.class), equalTo("000040"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.AMOUNT, String.class), equalTo("000000000124"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.TRANSMISSION_DATE_TIME, String.class), equalTo("0802152221"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.SYSTEM_TRACE_AUDIT_NUMBER, String.class), equalTo("000101"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.DATE_TIME_LOCAL_TRANSACTION, String.class), equalTo("210802152221"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.DATE_EXPIRATION, String.class), equalTo("2010"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.POINT_OF_SERVICE_DATA_CODE, String.class), equalTo("V69999000000"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.FUNCTION_CODE, String.class), equalTo("101"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.CARD_ACCEPTOR_BUSINESS_CODE, String.class), equalTo("1520"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.ACQUIRER_INSTITUTION_ID_CODE, String.class), equalTo("456654120087"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.RETRIEVAL_REFERENCE_NUMBER, String.class), equalTo("099920700619"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.CARD_ACCEPTOR_TERMINAL_IDENTIFICATION, String.class), equalTo("43214321"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.CARD_ACCEPTOR_ID_CODE, String.class), equalTo("455490051556622"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.CARD_ACCEPTOR_NAME_LOCATION, String.class), equalTo("Welch & Cie\\Geraldine Lane\\New York\\11590"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.CURRENCY_CODE, String.class), equalTo("978"));
+            MatcherAssert.assertThat(messageData.get(DummyDataElement.PERSONAL_IDENTIFICATION_DATA), equalTo(expectedPersonalIdentificationData));
+
+        }
+
+    }
+
+}
